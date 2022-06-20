e4964014ab84abcdf32370da08323b1ab104bb0b
runhopeS <74136669+runhopes@users.noreply.github.com>
2022-06-20 16:03:42 +0300

    Add files via upload

---
+++ php_ForWhileFunction.php
@@ -0,0 +1,28 @@
+<?php
+
+function UcgenYazdir($deger)
+{
+	for($i=1;$i<$deger;$i++)
+	{
+		$sayac=0;
+		while($sayac < $i)
+		{
+			echo "0";
+			$sayac++;
+		}
+		echo "<br>";
+
+	}
+
+}
+
+
+
+UcgenYazdir(15);
+
+
+
+
+
+
+?>
